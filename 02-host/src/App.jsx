2b13a3c3fe85aec23e6a5008969d491222a54a55
dmaestre <dmaestre@bancocaminos.es>
2023-07-14 14:38:33 +0200

    set up both host and consumed projects

---
+++ 02-host/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+const App = () => (
+	<>
+		<Navbar />
+	</>
+);
+
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
