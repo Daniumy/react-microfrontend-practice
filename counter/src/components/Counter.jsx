73afa269a1f1ef4b87bf17ffd619528e8cb00e57
dmaestre <dmaestre@bancocaminos.es>
2023-07-15 15:03:07 +0200

    dynamic styled components

--- counter/src/components/Counter.jsx
+++ counter/src/components/Counter.jsx
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
 import "./Counter.css";
 import Button from "./Button";
+import Styled from "@emotion/styled";
+
+const Success = Styled.span`
+	color: green;
+`;
+
+const Error = Styled.span`
+	color: red;
+`;
 
 const Counter = () => {
 	const [count, setCount] = React.useState(0);
@@ -8,7 +17,10 @@ const Counter = () => {
 	// If CSS is going to be used, better to call the class counter-container as this css is going to apply to the host's "container" class as well
 	return (
 		<div className="container">
-			<h2>Counter: {count}</h2>
+			<h2>
+				Counter:{" "}
+				{count >= 0 ? <Success>{count}</Success> : <Error>{count}</Error>}
+			</h2>
 			<Button onClick={() => setCount(count + 1)} text={"+1"} />
 			<Button onClick={() => setCount(count - 1)} text={"-1"} />
 		</div>
